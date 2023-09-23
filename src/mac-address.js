9f57d0845e2790492f2f89dfe658ce874b062a3b
KatrinaShr <katrineshr@gmail.com>
2023-09-23 23:26:49 +0300

    add: getMatrixElementsSum

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let hexadecimalDigits = '0123456789ABCDEF-'.split('');
+  let arrN = n.split('');
+
+  for (let i = 0; i < arrN.length; i++) {
+    if (!hexadecimalDigits.find(item => item === arrN[i])) { 
+      return false;
+    }
+  }
+  return true;
 }
+
 module.exports = {
   isMAC48Address
 };
